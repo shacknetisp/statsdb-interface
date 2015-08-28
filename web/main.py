68864fa5b21b8f255369c7eb1587ac87ae3727b0
Blank <shacknetisp@hotmail.com>
2015-08-28 00:07:10 -0400

    Show top player

--- web/main.py
+++ web/main.py
@@ -3,6 +3,7 @@ from . import base
 import api
 import dbselectors
 from .base import tdlink
+from .base import alink
 import timeutils
 
 
@@ -29,9 +30,24 @@ def page(sel):
             game["timeplayed"]))
         recentgames += '<td>%s</td>' % timeutils.agohtml(game["time"])
         recentgames += '</tr>'
+    players = {}
+    for gid, game in list(reversed(list(games.items())))[:25]:
+        if game["mode"] != 6:
+            for player in game["players"]:
+                if player["handle"]:
+                    if player["handle"] not in players:
+                        players[player["handle"]] = 0
+                    players[player["handle"]] += player["score"]
+    if players:
+        best = sorted(list(players.items()), key=lambda x: -x[1])[0]
+        topplayer = "<h3>Top player from recent games: <b>%s</b> [%d]</h3>" % (
+            alink('player', best[0], best[0]), best[1])
+    else:
+        topplayer = ""
     ret = """
     <h2>Overview</h2>
     <div class='display-table'>
+        {topplayer}
         <h3>Recent Games</h3>
         <table>
             <tr>
@@ -45,5 +61,6 @@ def page(sel):
             {recentgames}
         </table>
     </div>
-    """.format(recentgames=recentgames)
+    """.format(recentgames=recentgames,
+        topplayer=topplayer)
     return base.page(sel, ret, title="Overview")
